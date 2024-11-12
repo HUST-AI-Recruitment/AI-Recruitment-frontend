59025f6bde84666d0c71d92012fbf6c2fe819287
yali-hzy <yali-hzy@qq.com>
2024-11-12 09:16:40 +0800

    fix: alert("token expired")

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,7 @@
 const checkTokenExpire = () => {
   const expire = parseInt(localStorage.getItem('expire')) || null
   if (expire !== null && Date.now() > expire) {
-    alert('Token expired')
+    alert('登录已过期，请重新登录')
     return true
   }
   return false
